6860e45667a25215377228dc58165132abfaae37
dulan71 <dulansachintha71x@gmail.com>
2025-02-11 22:05:50 +0530

    day 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 
 import './App.css'
 import ProductCard from './components/productCard'
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
 
 function App() {
   
 
   return (
-    <div>
-     
-      <ProductCard name="JBL mini3" price="3250/-"
-      description ="this product is good"
-      photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQohjiYZfl8iHWpol0pnwrpqMwLbQzBfDLPw&s"
-      />
-
-      <ProductCard name ="JBL" price ="4750/-"
-      description = "this is a jbl product"
-      photoUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn5zwgheO1XpkeVFTwqTxilMGDpiQ95QBwRw&s"
-      />
-
-      <ProductCard name = "JBL speaker" price = "2750/-"
-      description = "this is a jbl mini speaker"
-      photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbSprFv44U0fULz8gXh1UrspLlQpqWrpQlrA&s"
-      />
+    <div className='w-full h-screen flex'>
+      <div className='w-[400px] h-full bg-green-200'>
+        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
+          <BsGraphDown/>
+          Dashboard
+        </button>
+        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
+          <FaRegBookmark/>
+          Bookings
+        </button>
+        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
+          <MdSpeaker/>
+          Items
+        </button>
+        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
+          <FaRegUser/>
+          Users
+        </button>
+       </div>
+      
+      <div className='w-full bg-red-600'>
+        
+      </div>
+  
     </div>
 
   )
