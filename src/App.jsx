5e74d3d093f33a259c41d3a92fa7056aca20bb13
dulan71 <dulansachintha71x@gmail.com>
2025-02-14 00:18:26 +0530

    Day-11

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import ProductCard from './components/productCard'
-import { BsGraphDown } from "react-icons/bs";
-import { FaRegBookmark } from "react-icons/fa";
-import { MdSpeaker } from "react-icons/md";
-import { FaRegUser } from "react-icons/fa";
+
+
+import HomePage from './pages/home/homePage';
+import AdminPage from './pages/admin/adminPage';
 
 function App() {
   
 
   return (
-    <div className='w-full h-screen flex'>
-      <div className='w-[400px] h-full bg-green-200'>
-        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
-          <BsGraphDown/>
-          Dashboard
-        </button>
-        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
-          <FaRegBookmark/>
-          Bookings
-        </button>
-        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
-          <MdSpeaker/>
-          Items
-        </button>
-        <button className='w-full h-[40px] text[25px] font-bold flex justify-center items-center'>
-          <FaRegUser/>
-          Users
-        </button>
-       </div>
-      
-      <div className='w-full bg-red-600'>
-        
-      </div>
-  
-    </div>
+    <BrowserRouter>
+    <Routes path="/*">
+    <Route path = "/admin/*" element={<AdminPage/>}/>
+    <Route path="/*" element={<HomePage/>}/>
+    <Route path="/*" element={<h1>Not Found</h1>}/>
+    </Routes>
+    </BrowserRouter>  
+
 
   )
 }
---
+++ src/pages/home/homePage.jsx
@@ -0,0 +1,28 @@
+
+import { Route, Routes } from "react-router-dom";
+import Header from "../../components/header";
+import Items from "./items";
+import Home from "./home";
+import Gallery from "./gallery";
+import Contact from "./contact";
+import ErrorNot from "./error";
+
+
+export default function HomePage(){
+    return(
+        <>
+      <Header/>
+      
+    <div className = "h-[calc(100vh-100px)] w-full">
+        <Routes path="/*">
+            <Route path = "/contact" element={<Contact/>}/>
+            <Route path = "/gallery" element={<Gallery/>}/>
+            <Route path="/items" element={<Items/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/*" element={<ErrorNot/>}/>
+        </Routes>
+
+    </div>
+    </>
+    )
+}
